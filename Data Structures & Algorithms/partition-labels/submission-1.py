5740a5f1d2d3bdd8f0f20c0767110450f2e665ce
yuangc3 <102508674+yuangc3@users.noreply.github.com>
2026-07-21 21:56:21 -0500

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        #split into substring 
+        #each letter apprear in at most one substring 
+        last = {}
+
+        for i, char in enumerate(s):
+            last[char] = i
+        
+
+        result = []
+        start = 0
+        end = 0
+
+        for i, char in enumerate(s):
+            end = max(end, last[char])
+
+            if i == end:
+                length = end -start + 1
+                start = end+1
+                result.append(length)
+        return result
