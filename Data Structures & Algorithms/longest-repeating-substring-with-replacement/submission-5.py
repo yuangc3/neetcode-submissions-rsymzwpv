e777213fe0bb1700126803239612dabccaa69bd9
yuangc3 <102508674+yuangc3@users.noreply.github.com>
2026-09-09 22:44:30 -0500

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        temp =defaultdict(int)
+        l = 0 
+        
+        res = 0
+
+
+        for r in range(len(s)):
+            temp[s[r]]+=1
+            while (r-l+1) - max(temp.values()) > k:
+                temp[s[l]]-=1
+                l+=1
+            res = max(res, r-l+1)
+            print(res)
+        return res
