bbe164f8462a780de7b2bf80f96eabda856bb4f1
yuangc3 <102508674+yuangc3@users.noreply.github.com>
2026-09-09 23:41:38 -0500

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        temp = defaultdict(int)
+
+        for n in nums:
+            temp[n] += 1
+        
+        for value, count in temp.items():
+            if count > 1:
+                return value
+        
+        return 0
